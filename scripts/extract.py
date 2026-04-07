a04dc75959525dc297a8571018d5928fb58a17e1
albertogzga <albertognzga@gmail.com>
2026-04-07 16:46:08 -0600

    Added a script that shows the first five rows and describes the column data types

--- scripts/extract.py
+++ scripts/extract.py
@@ -11,9 +11,24 @@ def explore_data(file_path):
     
     # Load the dataset
     df = pd.read_csv(file_path, low_memory=False)
-    print("DataFrame shape")
+
+    # Print the shape
+    print("\nDataFrame shape")
     print(f"Rows: {df.shape[0]:,}")
     print(f"Columns: {df.shape[1]:,}")
+
+    # Print column names and data types
+    print("\n Columns and Data types")
+    print(df.dtypes)
+
+    # Print null counts
+    print("\nNull counts per column")
+    print(df.isnull().sum())
+
+    print("\nSample")
+    pd.set_option('display.max_columns', None)
+    print(df.head())
+
 
 if __name__ == "__main__":
     CSV_PATH = r"C:\Users\alber\OneDrive\Escritorio\DATA\Python\goverment-etl\data\raw\contract_awards_in_investment_project_financing.csv"
